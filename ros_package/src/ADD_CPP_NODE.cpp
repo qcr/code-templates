a15b42526cad86c4092bb3b6ce855b6e325f367e
Ben Talbot <b.talbot@qut.edu.au>
2021-04-20 09:23:44 +1000

    Resolve #3

--- ros_package/src/ADD_CPP_NODE.cpp
+++ ros_package/src/ADD_CPP_NODE.cpp
@@ -2,8 +2,9 @@
 #include "ros/ros.h"
 
 int main(int argc, char **argv) {
+  // Start your custom node (node logic should be added to the ADD_CPP_NODE__PASCAL 
+  // class in the PACKAGE_NAME library, not here)
   ros::init(argc, argv, "ADD_CPP_NODE");
-
   PACKAGE_NAME::ADD_CPP_NODE__PASCAL node;
   ros::spin();
 
