74d069aeb4f340fa81717518b3fc8af76d52d1be
Ben Talbot <b.talbot@qut.edu.au>
2021-08-30 14:17:48 +1000

    Add python_package template with basic setup.py

---
+++ python_package/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='PACKAGE_NAME',
+      version='PACKAGE_VERSION',
+      author='AUTHOR_NAME',
+      author_email='AUTHOR_EMAIL',
+      url='https://github.com/best-of-acrv/refinenet',
+      description='RefineNet semantic image segmentation',
+      license_files=['LICENSE_FILE'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=PACKAGE_DEPENDENCIES,
+      TEMPLATE_START ADD_ENTRYPOINT
+      entry_points={'console_scripts': ['PACKAGE_NAME=PACKAGE_NAME.__main__:main']},
+      TEMPLATE_END
+      classifiers=(
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
