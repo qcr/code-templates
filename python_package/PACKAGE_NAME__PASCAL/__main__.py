d8f9f9dca0d481ab0aed50009e76bc2b81363331
Ben Talbot <b.talbot@qut.edu.au>
2021-08-30 14:45:37 +1000

    Finish basic python_package template (untested)

---
+++ python_package/PACKAGE_NAME__PASCAL/__main__.py
@@ -0,0 +1,12 @@
+from .PACKAGE_NAME__PASCAL import PACKAGE_NAME__CAMEL
+
+
+def main():
+    print("Install your package with 'pip install' (e.g. 'pip install -e .').")
+    print(
+        "This script will now be on your PATH, and runnable simply by 'ADD_ENTRYPOINT'"
+    )
+
+
+if __name__ == '__main__':
+    main()
--- python_package/setup.py
+++ python_package/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='PACKAGE_NAME',
+setup(name='PACKAGE_NAME__PASCAL',
       version='PACKAGE_VERSION',
       author='AUTHOR_NAME',
       author_email='AUTHOR_EMAIL',
@@ -15,7 +15,7 @@ setup(name='PACKAGE_NAME',
       packages=find_packages(),
       install_requires=PACKAGE_DEPENDENCIES,
       TEMPLATE_START ADD_ENTRYPOINT
-      entry_points={'console_scripts': ['PACKAGE_NAME=PACKAGE_NAME.__main__:main']},
+      entry_points={'console_scripts': ['ADD_ENTRYPOINT=PACKAGE_NAME__PASCAL.__main__:main']},
       TEMPLATE_END
       classifiers=(
           "Development Status :: 4 - Beta",
